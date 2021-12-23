1d5b3292dbec775373dbf662415d0f047c9205bc
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2021-12-23 17:40:04 +0100

    Controlling force depending on the press time. Cleaning up ball controller script.

--- Ping Ball/Assets/Scripts/BallController.cs
+++ Ping Ball/Assets/Scripts/BallController.cs
@@ -1,37 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float launchingForce = 35f;
-    public float pullingForce = 12f;
-    public float leftDrag = 1.8f;
+    private Rigidbody rb;
+    // Launching Vars
+    private float baseLaunchingForce = 20f;
+    private float currentForceMag;
+    private bool launch = false;
     private bool isLaunched = false;
+    private float holdDownStartTime;
+    // Gravity Vars
+    private float pullingForce = 12f;
+    // DTL Effect Vars
+    private float minLeftDrag = 0.4f;
+    private float maxLeftDrag = 1.8f;
     
 
-    private void awake() {
-        
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update() {
+        if (Input.GetButtonDown("Jump")) 
+        {
+            holdDownStartTime = Time.time;
+        }
 
+        if (Input.GetButtonUp("Jump"))
+        {
+            launch = true;
+        }
     }
 
     private void FixedUpdate() {
 
-        rb.AddForce(Vector3.forward * -1f * pullingForce * Time.deltaTime * 60f);
+        if (isLaunched)
+        {
+            ApplyGravity();
+        }
 
-        if (rb.velocity.z < 1f && isLaunched)
+        if (rb.velocity.z < 1f && rb.velocity.z > -0.6f && isLaunched)
         {
-            rb.AddForce(Vector3.right * -1f * leftDrag * Time.deltaTime * 60f);
+            ApplyDraggingToLeft();
         }
-        
-        if (Input.GetButton("Jump") && !isLaunched)
+
+        if (launch && !isLaunched)
         {
-            rb.AddForce(Vector3.forward * launchingForce * Time.deltaTime * 60f, ForceMode.Impulse);
-            isLaunched = true;
+            LaunchBall();
         }
+    }
+
+    private void LaunchBall()
+    {
+        float holdDownTime = Time.time - holdDownStartTime;
+        currentForceMag = baseLaunchingForce * holdDownTime;
+
+        if (currentForceMag > 35f)
+        {
+            currentForceMag = 35f;
+        }
+
+        rb.AddForce(Vector3.forward * currentForceMag * Time.deltaTime * 60f, ForceMode.Impulse);
+        isLaunched = true;
+
+        launch = false;
+    }
+
+    private void ApplyGravity()
+    {
+        rb.AddForce(Vector3.forward * -1f * pullingForce * Time.deltaTime * 60f);
+    }
+
+    private void ApplyDraggingToLeft()
+    {
+        rb.AddForce(Vector3.right * -1f * Random.Range(minLeftDrag, maxLeftDrag) * Time.deltaTime * 60f);
     }
 }
